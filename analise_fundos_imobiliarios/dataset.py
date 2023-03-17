65ebcbc794661da2a77d2e2975e3dbce2202563d
Alex Prestes <alex.prestes@usp.br>
2023-03-17 12:55:36 -0300

    criação do dataset

---
+++ analise_fundos_imobiliarios/dataset.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pandas as pd
+
+from analise_fundos_imobiliarios.series import (
+    fbm_negative,
+    fbm_none,
+    fbm_positive,
+    logistic_map,
+    random,
+    sine_map,
+)
+
+
+class Dataset:
+    def __init__(self, n=20, L0=10, L=60):
+        self.n = n
+        self.L0 = L0
+        self.L = L
+        self.list_series = [
+            fbm_negative,
+            fbm_none,
+            fbm_positive,
+            logistic_map,
+            random,
+            sine_map,
+        ]
+
+    def run(self):
+        list_features = []
+        list_target = []
+
+        for i in range(self.L0, self.L):
+            for _ in range(self.n):
+                for func in self.list_series:
+                    ts = func(i)
+                    list_features.append(ts)
+                    list_target.append(str(func.__qualname__))
+
+        self.data = pd.DataFrame(
+            pd.Series(list_features), columns=['time_series']
+        )
+        self.target = np.asarray(list_target)
+
+    def set_series(self, series=[logistic_map, fbm_positive]):
+        self.list_series = series
---
+++ analise_fundos_imobiliarios/series.py
@@ -0,0 +1,47 @@
+import numpy as np
+from fbm import fbm
+from numpy._typing import NDArray
+
+
+def logistic_map(n: int = 2, r: float = 4.0) -> NDArray:
+    """
+    Gera uma série temporal do mapa logistico
+
+    Parameters:
+        n: Comprimento da série
+        r: Taxa de crescimento
+
+    Returns:
+        Uma lista com os valores da série
+    """
+    x_serie = np.zeros(n)
+    x_serie[0] = np.random.random()
+
+    for i in range(1, n):
+        x_serie[i] = r * x_serie[i - 1] * (1 - x_serie[i - 1])
+    return x_serie
+
+
+def sine_map(n: int = 2, r: float = 1.0) -> NDArray:
+    x_serie = np.zeros(n)
+    x_serie[0] = np.random.random()
+
+    for i in range(1, n):
+        x_serie[i] = r * np.sin(np.pi * x_serie[i - 1])
+    return x_serie
+
+
+def fbm_positive(n: int = 2, hurst: float = 0.7) -> NDArray:
+    return fbm(n=(n - 1), hurst=hurst)
+
+
+def fbm_negative(n: int = 2, hurst: float = 0.2) -> NDArray:
+    return fbm(n=(n - 1), hurst=hurst)
+
+
+def fbm_none(n: int = 2, hurst: float = 0.5) -> NDArray:
+    return fbm(n=(n - 1), hurst=hurst)
+
+
+def random(n: int = 2) -> NDArray:
+    return np.random.random(n)
