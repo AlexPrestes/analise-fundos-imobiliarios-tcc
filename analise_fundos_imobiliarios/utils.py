e79c93c87d450a4b05ceb2755d7989a099434a4c
Alex Prestes <alex.prestes@usp.br>
2023-03-28 21:02:26 -0300

    decorador para facilitar a vetorização

---
+++ analise_fundos_imobiliarios/utils.py
@@ -0,0 +1,12 @@
+import networkx as nx
+import numpy as np
+
+
+def vectorize_metric(func):
+    def inner1(*args, **kwargs):
+        if isinstance(args[0], nx.Graph):
+            return func(*args, **kwargs)
+        else:
+            return np.vectorize(func, signature='()->()')(*args, **kwargs)
+    
+    return inner1
